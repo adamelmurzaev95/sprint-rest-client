d2f5302d04cfef0faad022cce0dd0aa96e64c51d
Adam <adamibnkhavaj@gmail.com>
2021-05-17 17:12:58 +0500

    Add one test

--- src/main/java/com/elmurzaev/rest_client/entity/Employee.java
+++ src/main/java/com/elmurzaev/rest_client/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.elmurzaev.rest_client.entity;
 
+import java.util.Objects;
+
 public class Employee {
     private int id;
     private String name;
@@ -66,5 +68,18 @@ public class Employee {
                 ", department='" + department + '\'' +
                 ", salary=" + salary +
                 "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && salary == employee.salary && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname) && Objects.equals(department, employee.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, department, salary);
     }
 }
--- src/test/java/com/elmurzaev/rest_client/AppTest.java
+++ src/test/java/com/elmurzaev/rest_client/AppTest.java
@@ -2,19 +2,30 @@ package com.elmurzaev.rest_client;
 
 import static org.junit.Assert.assertTrue;
 
+import com.elmurzaev.rest_client.entity.Employee;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private static Communication communication;
+
+    @BeforeClass
+    public static void init(){
+        communication = new Communication(new RestTemplate());
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldEmployeesBeEqual()
     {
-        assertTrue( true );
+        Employee employee = new Employee("Adam", "Elmurzaev", "IT", 170000);
+        employee.setId(4);
+        Assert.assertEquals(employee, communication.show(4));
     }
 }
